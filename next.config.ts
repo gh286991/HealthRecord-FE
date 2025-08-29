aa773f33dbaf4435a9f13cfb025c790b8c0ec2e6
gh286991 <gh286991@gmail.com>
2025-08-30 02:33:05 +0800

    fix:update

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
-      urlPattern: /^https?.*/, 
+      urlPattern: /^https:\/\/tomminio-api\.zeabur\.app\/.*/, 
       handler: 'NetworkFirst',
       options: {
         cacheName: 'offlineCache',
